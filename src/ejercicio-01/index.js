797a473ed0cd9107aa2b87bb28e606d298349b47
Fernando <fernando30041972@hotmail.com>
2023-11-06 23:01:07 +0100

    Añadir ejercicio 01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,10 @@
 // Escribe tu código aquí:
+const person = {
+    name: 'Juan',
+    age: 45,
+    city: 'Sevilla'
+}
+
+console.log('person.Juan')
+console.log(person['age'])
+console.log( person['city'])
